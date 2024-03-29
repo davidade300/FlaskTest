4c2ba09f6c304d054b679afa5a1809b68e311a9c
davidade300 <iaderinho@outlook.com>
2024-03-29 19:10:00 -0300

    app.py fixed, first html page created.

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+@app.route('/inicio')
+def ola():
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Ataria')
+    jogo2 = Jogo('God of War', "Hack n'slash", 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html',
+                           titulo='Jogos',
+                           jogos=lista)
 
 
 if __name__ == '__main__':
