cc4d8b22cb97dcaf935de10f4e182fdf042dac4e
davidade300 <iaderinho@outlook.com>
2024-03-30 18:05:52 -0300

    new pages added

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect
 
 
 class Jogo:
@@ -10,16 +8,35 @@ class Jogo:
         self.console = console
 
 
-@app.route('/inicio')
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', "Hack n'slash", 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__)
+
+
+@app.route('/')
 def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Ataria')
-    jogo2 = Jogo('God of War', "Hack n'slash", 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html',
                            titulo='Jogos',
                            jogos=lista)
 
 
+@app.route("/novo")
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route("/criar", methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    #  jogo = Jogo(nome, categoria, console)
+    lista.append(Jogo(nome, categoria, console))
+    return redirect('/')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
